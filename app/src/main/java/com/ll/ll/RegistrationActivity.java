2dc8fb561e68ce1b993984cfbbb30a56612e10dd
shivakumar-p12 <shivakumar.p12@gmail.com>
2015-12-21 09:56:02 +0530

    Working Registration activity. Inserting only a row in DB. On insert replacing old with new one

--- app/src/main/java/com/ll/ll/RegistrationActivity.java
+++ app/src/main/java/com/ll/ll/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.ll.ll;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,21 +10,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
-    EditText name,age,username,password;
+    EditText rname,rage,username,password;
+    Context context=this;
+    UserDbHelper userDbHelper;
+    SQLiteDatabase sqlliteDatabase;
     Button newregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-        EditText name = (EditText)findViewById(R.id.rname);
-        EditText age = (EditText)findViewById(R.id.rage);
-        EditText username = (EditText)findViewById(R.id.runame);
-        EditText password = (EditText)findViewById(R.id.rpwds);
-        Button newregister = (Button)findViewById(R.id.registerme);
+         rname = (EditText)findViewById(R.id.rname);
+         rage = (EditText)findViewById(R.id.rage);
+         username = (EditText)findViewById(R.id.runame);
+         password = (EditText)findViewById(R.id.rpwds);
+         newregister = (Button)findViewById(R.id.registerme);
     }
     public void registerNew (View v){
-        startActivity(new Intent(this, HomeActivity.class));
+        String name= rname.getText().toString();
+        int age = Integer.parseInt(rage.getText().toString());
+        String uname= username.getText().toString();
+        String pswd=password.getText().toString();
+        userDbHelper=new UserDbHelper(context);
+        sqlliteDatabase=userDbHelper.getWritableDatabase();
+        userDbHelper.addinnformation(name,age,uname,pswd,sqlliteDatabase);
+        Toast.makeText(getBaseContext(), "Data saved", Toast.LENGTH_LONG).show();
+        userDbHelper.close();
+        //startActivity(new Intent(this, HomeActivity.class));
     }
 }
---
+++ app/src/main/java/com/ll/ll/User.java
@@ -0,0 +1,18 @@
+package com.ll.ll;
+
+/**
+ * Created by rdm-09 on 19-Dec-15.
+ */
+public class User {
+
+
+        public static abstract class NewUserInfo
+        {
+            public static final String NAME="name";
+            public static final String AGE="age";
+            public static final String USER_NAME="user_name";
+            public static final String USER_PSWD="user_pswd";
+            public static final String TABLE_NAME="user_info";
+        }
+
+}
